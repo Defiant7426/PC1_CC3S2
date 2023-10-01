e8e542be4ebda83fde4f8dd00bc0bf6c130dbb7c
Luis <pldelacruzv@gmail.com>
2023-10-01 16:58:05 -0500

    Primera PC DS

--- app.rb
+++ app.rb
@@ -1,7 +1,28 @@
 require 'sinatra' 
-class MyApp < Sinatra::Base 
-get '/' do 
-"<!DOCTYPE html><html><head></head><body><h1>Otro Mensaje</h1></body></html>" 
-end 
+require_relative 'word_guesser_game'
+
+# class MyApp < Sinatra::Base 
+# get '/' do 
+# "<!DOCTYPE html><html><head></head><body><h1>Otro Mensaje</h1></body></html>" 
+# end 
+# end
+
+enable :sessions
+
+post '/create' do
+	session[:game] = WordGuesserGame.new('palabra_aleatoria')
+	redirect '\show'
+end
+
+post '/show' do
+	@game = session[:game]
+	erb :show
+end
+
+post '/guess' do
+	@game = session[:game]
+	@game.guess(params[:letter])
+	session[:game] = @game
+	redirect '/show'
 end
 
